37e60d99dcdc451fbddee9fc2b1bd308bf833c9e
Seth Corbett <scorbz9@gmail.com>
2021-12-13 13:28:15 -0800

    wrote GET /users/sign-up + created sign-up form

--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const { csrfProtection, asyncHandler } = require('./utils');
 
 /* GET /sign-up page */
-router.get('/sign-up', (req, res, next) => {
-
+router.get('/sign-up', csrfProtection, (req, res, next) => {
+  res.render('user-sign-up', { csrfToken: req.csrfToken(), title: 'Sign Up' } )
 });
 
 /* POST /sign-up - Register user */
-router.post('/sign-up', async (req, res, next) => {
+router.post('/sign-up', csrfProtection, async (req, res, next) => {
 
 });
 
 /* GET /login page */
-router.get('/login', async (req, res, next) => {
+router.get('/login', csrfProtection, async (req, res, next) => {
   res.send('respond with a resource');
 });
 
 /* POST /login - Perform login */
-router.post('/login', async (req, res, next) => {
+router.post('/login', csrfProtection, async (req, res, next) => {
 
 });
 

c8c927aa7601f7d4df56d5042f27ad7badfa4b71
Grant Russell <grant.ellis.russell@gmail.com>
2021-12-13 17:28:18 -0600

    jinglelist and reviews and associations

--- db/models/list.js
+++ db/models/list.js
@@ -9,6 +9,7 @@ module.exports = (sequelize, DataTypes) => {
     List.belongsTo(models.User, {
       foreignKey: 'userId'
     })
+    List.belongsTo(models.Jinglelist, { foreignKey: 'listId'});
   };
   return List;
 };

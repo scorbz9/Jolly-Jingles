3b3a8f703fd062f478ec9bc6e7bbd4fca5bc4e98
Seth Corbett <scorbz9@gmail.com>
2021-12-13 12:57:52 -0800

    defined login/logout/register routes + added views for login + register

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+/* GET /sign-up page */
+router.get('/sign-up', (req, res, next) => {
+
+});
+
+/* POST /sign-up - Register user */
+router.post('/sign-up', async (req, res, next) => {
+
+});
+
+/* GET /login page */
+router.get('/login', async (req, res, next) => {
   res.send('respond with a resource');
 });
+
+/* POST /login - Perform login */
+router.post('/login', async (req, res, next) => {
+
+});
+
+/* POST /logout - Perform logout */
+router.post('/logout', async (req, res, next) => {
+
+});
+
+
 
 module.exports = router;

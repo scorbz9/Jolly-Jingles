ce43e5cd2458fcbee81cf4087b3e649a71ffd798
Robert <Robertpopphan@gmail.com>
2021-12-13 16:52:46 -0500

    working on sign-in form

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const { csrfProtection, asyncHandler } = require('./utils');
 
 /* GET /sign-up page */
 router.get('/sign-up', csrfProtection, (req, res, next) => {
-  res.render('user-sign-up', { csrfToken: req.csrfToken(), title: 'Sign Up' } )
+  res.render('user-sign-up', { csrfToken: req.csrfToken(), title: 'Sign up' } )
 });
 
 /* POST /sign-up - Register user */
@@ -12,18 +12,21 @@ router.post('/sign-up', csrfProtection, async (req, res, next) => {
 
 });
 
-/* GET /login page */
-router.get('/login', csrfProtection, async (req, res, next) => {
-  res.send('respond with a resource');
+/* GET /sign-in page */
+router.get('/sign-in', csrfProtection, async (req, res, next) => {
+  res.render('user-sign-in', {
+    title: 'Sign in',
+    csrfToken: req.csrfToken()
+  })
 });
 
-/* POST /login - Perform login */
-router.post('/login', csrfProtection, async (req, res, next) => {
+/* POST /sign-in - Perform login */
+router.post('/sign-in', csrfProtection, async (req, res, next) => {
 
 });
 
-/* POST /logout - Perform logout */
-router.post('/logout', async (req, res, next) => {
+/* POST /sign-out - Perform logout */
+router.post('/sign-out', async (req, res, next) => {
 
 });
 

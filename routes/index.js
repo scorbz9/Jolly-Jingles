023d02985cf615e931ff3f7a99a119dab7505639
Robert <Robertpopphan@gmail.com>
2021-12-14 18:33:55 -0500

    working on explore

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const {Jingle} = require('../db/models');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Explore' });
-});
+const asyncHandler = (handler) => {
+  return (req, res, next) => {
+      return handler(req, res, next).catch(next);
+  };
+};
+
+router.get('/', asyncHandler(async(req, res) => {
+  const jingles = await Jingle.findAll()
+  console.log(jingles)
+  const { image, name, artist, avgRating} = req.body;
+  res.render('explore', {title: 'explore', jingles })
+}))
 
 module.exports = router;

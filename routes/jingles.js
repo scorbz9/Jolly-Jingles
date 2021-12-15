242dfb5754c023e46eff93eed53b91a2552b9b2b
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2021-12-15 12:40:18 -0800

    setting up router jingles reviews router, in progress

--- routes/jingles.js
+++ routes/jingles.js
@@ -6,15 +6,19 @@ const { csrfProtection, asyncHandler } = require('./utils');
 
 // to see the info page for a jingle
 router.get('/:id(\\d+)', asyncHandler(async (req, res) => {
-    console.log(req.params)
+    // console.log(req.params)
     const id = parseInt(req.params.id, 10);
-    console.log(req.params.id)
-    console.log(id)
+    // console.log(req.params.id)
+    // console.log(id)
     const jingle = await db.Jingle.findByPk(id);
-    console.log(jingle)
+
+    // console.log(jingle)
+
     if (jingle) {
-    console.log(jingle.name);
-    res.render('index', {title: jingle.name})
+        const { image, artist, name, lyrics, avgRating, description } = jingle;
+
+
+    res.render('index', {title: jingle.name, image, artist, name, lyrics, avgRating, description})
     } else {
         res.status(404)
         res.send('Page Not Found');

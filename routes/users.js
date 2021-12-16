e445b951e31f9fe7244ba2d72b3e1d50dc19dce6
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2021-12-16 09:54:18 -0800

    updated footer pug file

--- routes/users.js
+++ routes/users.js
@@ -150,26 +150,10 @@ router.get('/:userId(\\d+)/jingleLists', csrfProtection, asyncHandler(async (req
 
   const user = await db.User.findByPk(userId);
 
-<<<<<<< HEAD
-  // TODO - Get user's default 'My Jingles' Jinglelist - below is placeholder listId
-  const lists = await db.List.findAll({ where: { userId }})
-  // console.log(lists)
-
-  const listId = lists.map(list => list.id)[1]
-  // console.log(listId)
-
-  //get jingleList with jingleId
-const jingleList = await db.Jinglelist.findAll({ where: { listId }});
-
-
-  console.log(jingleList)
-
-=======
   // More database configuration is required - need to add a marker to track which jinglelist is the user's default collection ('My Jingles')
   // TODO - Get user's default 'My Jingles' Jinglelist - below is placeholder listId
   const lists = await db.List.findAll({ where: { userId }})
   const listId = lists.map(list => list.id)[0]
->>>>>>> c374183aa0af55ff5330c5a39b421dfd8a72a100
 
 
   //array of jingles
@@ -179,33 +163,14 @@ const jingleList = await db.Jinglelist.findAll({ where: { listId }});
   });
 
 
-<<<<<<< HEAD
-// var artist = '';
-
-  // for (let i = 0; i < jingles.length; i++) {
-  //     artist += jingles[i].artist
-  // }
-
-  // jingles.forEach(async (jingle) => {
-  //    const jingleId = jingle.jingleId;
-  //    const single = await db.Jingle.findByPk(jingleId);
-
-  //    var image = single.image
-  //   //  var title = single.name
-  //   //  var avgRating = single.avgRating
-  //   //  var dateAdded = single.dateAdded
-  //     console.log(single.artist)
-=======
   jingles.forEach(async (jingle) => {
       // console.log(jingle.jingleId)
      const jingleId = jingle.jingleId;
      const single = await db.Jingle.findByPk(jingleId);
->>>>>>> c374183aa0af55ff5330c5a39b421dfd8a72a100
 
 
-  // });
+  });
 
-<<<<<<< HEAD
   res.render('user-jinglelists.pug', {
 
     csrfToken: req.csrfToken(),
@@ -214,17 +179,6 @@ const jingleList = await db.Jinglelist.findAll({ where: { listId }});
     lists,
     jingles,
     userId
-=======
-       csrfToken: req.csrfToken(),
-       title: 'My Jingles',
-       user,
-       jingles,
-       jingleList,
-       image
-       // list
-     });
-
->>>>>>> c374183aa0af55ff5330c5a39b421dfd8a72a100
   });
 
 }));

b1f9a5f89a1fcf93d4a6d965b7483b6791e4911c
Grant Russell <grant.ellis.russell@gmail.com>
2021-12-17 11:15:49 -0600

    fixed avg rating route on jingles page

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const {Jingle} = require('../db/models');
+const db = require('../db/models');
 
 const asyncHandler = (handler) => {
   return (req, res, next) => {
@@ -9,10 +9,32 @@ const asyncHandler = (handler) => {
 };
 
 router.get('/', asyncHandler(async(req, res) => {
-  const jingles = await Jingle.findAll()
+  // const jingles = await db.Jingle.findAll();
+  
   // console.log(jingles)
-  const { image, name, artist, avgRating} = req.body;
-  res.render('explore', {title: 'explore', jingles })
+  // // const reviews = await db.Review.findAll({ where: { jingles.id } })
+  // // const sumOfreviews = await db.Review.sum('rating');
+  // // const avgReviews = (sumOfreviews / reviews.length).toFixed(2);
+  // // const { image, name, artist, avgRating, id} = req.body;
+  // res.render('explore', {title: 'explore', jingles })
+
+  const jingles = await db.Jingle.findAll();
+  
+  jingles.forEach(async(jingle) => {
+    const reviews = await db.Review.findByPk(parseInt(jingle.id, 10));
+    console.log(reviews)
+    const sumOfReviews = await db.Review.sum('rating');
+    const avgReviews = (sumOfReviews / reviews.length).toFixed(2);
+    res.render('explore', {title: 'Explore', jingles, avgReviews})
+  })
+
+  
+  // console.log(jingles)
+  // // const reviews = await db.Review.findAll({ where: { jingles.id } })
+  // // const sumOfreviews = await db.Review.sum('rating');
+  // // const avgReviews = (sumOfreviews / reviews.length).toFixed(2);
+  // // const { image, name, artist, avgRating, id} = req.body;
+  // res.render('explore', {title: 'explore', jingles, avgReviews})
 }))
 
 module.exports = router;

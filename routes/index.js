65b11d2ecde00b92a5e9ac18a8ca062ca753c8d2
Grant Russell <grant.ellis.russell@gmail.com>
2021-12-17 12:31:39 -0600

    added migration file to update jingles avgRating column

--- routes/index.js
+++ routes/index.js
@@ -9,34 +9,11 @@ const asyncHandler = (handler) => {
 };
 
 router.get('/', asyncHandler(async(req, res) => {
-  // const jingles = await db.Jingle.findAll();
-  
-  // console.log(jingles)
-  // // const reviews = await db.Review.findAll({ where: { jingles.id } })
-  // // const sumOfreviews = await db.Review.sum('rating');
-  // // const avgReviews = (sumOfreviews / reviews.length).toFixed(2);
-  // // const { image, name, artist, avgRating, id} = req.body;
-  // res.render('explore', {title: 'explore', jingles })
-
   const jingles = await db.Jingle.findAll();
-  
-  jingles.forEach(async(jingle) => {
-    const reviews = await db.Review.findByPk(parseInt(jingle.id, 10));
-    console.log(reviews.length)
-    if(reviews) {
-    const sumOfReviews = await db.Review.sum('rating', {where: jingle.id});
-    const avgReviews = (sumOfReviews / reviews.length).toFixed(2);
-    res.render('explore', {title: 'Explore', jingles, avgReviews})
-    }
-  })
 
-  
-  // console.log(jingles)
-  // // const reviews = await db.Review.findAll({ where: { jingles.id } })
-  // // const sumOfreviews = await db.Review.sum('rating');
-  // // const avgReviews = (sumOfreviews / reviews.length).toFixed(2);
-  // // const { image, name, artist, avgRating, id} = req.body;
-  // res.render('explore', {title: 'explore', jingles, avgReviews})
+  res.render('explore', {title: 'explore', jingles })
+
+
 }))
 
 module.exports = router;
